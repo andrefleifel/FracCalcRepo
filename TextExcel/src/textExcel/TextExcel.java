d34cc8d61f315992800e80989dfe06f804050cd4
andrefleifel <19fleifela63@stu.smuhsd.org>
2019-03-13 10:46:04 -0700

    update

---
+++ TextExcel/src/textExcel/TextExcel.java
@@ -0,0 +1,26 @@
+//@author Andre Fleifel
+
+package textExcel;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Update this file with your own code.
+
+public class TextExcel
+{
+
+	public static void main(String[] args) { 
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter a Command: ");
+		String userInput = scanner.nextLine();
+		Spreadsheet sheet = new Spreadsheet();
+		while (userInput.equals(equalsIgnoreCase("quit")) {
+			System.out.println(sheet.processCommand(userinput));
+			
+			System.out.println(
+					)
+			}
+		}
+    }
+}
